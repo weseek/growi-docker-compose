d96fcedfca2fed0989e46105175692c49d0f1be8
Yuki Takei <yuki@weseek.co.jp>
2018-07-29 01:15:05 +0900

    modify apply-growi-agent.sh for styles

--- hackmd/apply-growi-agent.sh
+++ hackmd/apply-growi-agent.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo '<script src="<%= process.env.GROWI_URI %>/_hackmd/load-styles"></script>' >> /hackmd/public/views/codimd/head.ejs
+echo '<link rel="stylesheet" type="text/css" href="<%= process.env.GROWI_URI %>/_hackmd/load-styles">' >> /hackmd/public/views/codimd/head.ejs
 echo '<script src="<%= process.env.GROWI_URI %>/_hackmd/load-agent" defer></script>' >> /hackmd/public/views/codimd/foot.ejs
