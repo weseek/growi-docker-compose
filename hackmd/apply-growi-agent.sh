2c7d1916073c3817b956b64cacc370017175461f
Yuki Takei <yuki@weseek.co.jp>
2024-11-12 10:09:22 +0000

    remove HackMD integration

--- hackmd/apply-growi-agent.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo '<script src="<%= process.env.GROWI_URI %>/_hackmd/load-styles"></script>' >> /codimd/public/views/codimd/head.ejs
-echo '<script src="<%= process.env.GROWI_URI %>/_hackmd/load-agent" defer></script>' >> /codimd/public/views/codimd/foot.ejs
